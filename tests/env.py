c579dea3204e87ffdb6db3d7fbbbbdc3fa5c2af9
Mathieu Reymond <mathieu.reymond@gmail.com>
2021-12-06 12:59:33 +0100

    use gym registry

--- tests/env.py
+++ tests/env.py
@@ -3,17 +3,14 @@ import os
 sys.path.append(os.getcwd())
 
 if __name__ == '__main__':
-    from ode_epi_model import OdeEpiModel
-    from envs.epi_env import EpiEnv
+    import gym
+    import envs
     import numpy as np
-
-    model = OdeEpiModel(2, np.array([1000, 0, 0., 1000, 0, 0]), .2, .2)
     
-    env = EpiEnv(model)
+    env = gym.make('EpiODEContinuous-v0')
     s = env.reset()
-    print(s)
-    breakpoint()
-    for _ in range(3):
+    d = False
+    while not d:
         a = env.action_space.sample()
         s, r, d, _ = env.step(a)
         print(s, r)
